a4dee3f779501dc02ecb18436ff4905f5edb1f5c
jake <jake@jakecoffman.com>
2019-03-24 13:55:24 -0500

    hard reload?

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; refreshing.')
-      window.location.reload()
+      window.location.reload(true)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
