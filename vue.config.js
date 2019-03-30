22839b6995e0683ca05f99c3104b9c39f089a65b
jake <jake@jakecoffman.com>
2019-03-30 11:46:38 -0500

    fix pwa auto-update?

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,16 @@ module.exports = {
     devServer: {
         proxy: {
             '/ws': {
-                // target: 'http://localhost:8222',
-                target: 'https://set.jakecoffman.com',
+                target: 'http://localhost:8222',
+                // target: 'https://set.jakecoffman.com',
                 changeOrigin: true,
                 ws: true
             }
+        }
+    },
+    pwa: {
+        workboxOptions: {
+            skipWaiting: true
         }
     }
 }
